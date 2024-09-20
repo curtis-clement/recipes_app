f1cbd40aebbeef2ca183d1701aa63279c2994a17
Curtis Clement <curtis.clement@wonderkind.com>
2024-09-20 17:17:37 +0200

    add stack routing to the categories page so that clicking on a meal category will navigate to a new meals page

--- lib/pages/categories_page.dart
+++ lib/pages/categories_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:recipes_app/data/dummy_data.dart';
 import 'package:recipes_app/widgets/category_grid_item.dart';
+import 'package:recipes_app/pages/meals_page.dart';
+import 'package:recipes_app/models/category.dart';
 
 class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key});
+
+  void _selectCateogry(BuildContext context, Category category) {
+    final meals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+
+    final route = MaterialPageRoute(builder: (ctx) => MealsPage(title: category.title, meals: meals));
+    Navigator.push(context, route); // Same as Navigator.of(context).push(route);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +30,12 @@ class CategoriesPage extends StatelessWidget {
           ),
           children: [
             for (final category in availableCategories)
-              CategoryGridItem(category: category),
+              CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCateogry(context, category);
+                },
+              ),
           ],
         ));
   }
