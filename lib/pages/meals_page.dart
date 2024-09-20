a1eb3c2e932411fa47155e674fc8b46c64017d66
Curtis Clement <curtis.clement@wonderkind.com>
2024-09-20 15:41:18 +0200

    add meals page and initial data provided

--- lib/pages/meals_page.dart
+++ lib/pages/meals_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_app/models/meal.dart';
 
 class MealsPage extends StatelessWidget {
-  const MealsPage({super.key});
+  const MealsPage({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => Text(meals[index].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          children: [
+            Text(
+              'No meals found...',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
